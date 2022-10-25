d8fca841662fd579b0942f2ea373c33336068c35
wellington gadelha <contato.informeai@gmail.com>
2022-10-25 18:32:49 -0300

    chore(dist/index.js): update code of index

--- dist/index.js
+++ dist/index.js
@@ -10982,6 +10982,15 @@ const command = __nccwpck_require__(1514);
 
 async function run(){
     try {
+        const sha = core.getInput('sha');
+        const tag = core.getInput('tag');
+        const ghToken = core.getInput('gh-token');
+        const appendTag = core.getInput('append-tag');
+
+        console.log('sha: ',sha);
+        console.log('tag: ',tag);
+        console.log('gh-token: ',ghToken);
+        console.log('append-tag: ',appendTag);
         
     } catch (error) {
       core.setFailed(error.message);
