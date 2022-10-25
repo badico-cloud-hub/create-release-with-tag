ba27338925cfe08051af0bd10ca99d0a03ac854a
wellington gadelha <contato.informeai@gmail.com>
2022-10-25 18:28:29 -0300

    chore(index.js): get inputs values

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const command = require('@actions/exec');
 
 async function run(){
     try {
+        const sha = core.getInput('sha');
+        const tag = core.getInput('tag');
+        const ghToken = core.getInput('gh-token');
+        const appendTag = core.getInput('append-tag');
+
+        console.log('sha: ',sha);
+        console.log('tag: ',tag);
+        console.log('gh-token: ',ghToken);
+        console.log('append-tag: ',appendTag);
         
     } catch (error) {
       core.setFailed(error.message);
