4801c8896493803e93560d4e4fec71cb08b223d1
wellington gadelha <contato.informeai@gmail.com>
2022-10-26 08:37:52 -0300

    chore(dist/index.js): add logic of create release

--- dist/index.js
+++ dist/index.js
@@ -10986,12 +10986,15 @@ async function run(){
         const tag = core.getInput('tag');
         const ghToken = core.getInput('gh-token');
         const appendTag = core.getInput('append-tag');
+        const octokit = github.getOctokit(ghToken);
 
         console.log('branch: ',branch);
         console.log('tag: ',tag);
         console.log('gh-token: ',ghToken);
         console.log('append-tag: ',appendTag);
         
+        const release = command.exec('gh',['release','create',`${tag}`,`--title=${tag}`,`--target=${branch}`,'--generate-notes'])
+        console.log('release result: ', release)
     } catch (error) {
       core.setFailed(error.message);
     }
