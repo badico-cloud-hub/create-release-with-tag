0c9afbb59733ace0d8065e12486c8f524301d8ba
wellington gadelha <contato.informeai@gmail.com>
2022-11-01 10:43:44 -0300

    chore(dist/index.js): add verifyInputs and update logic of add uppercase in name of append-tag

--- dist/index.js
+++ dist/index.js
@@ -10979,6 +10979,19 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const command = __nccwpck_require__(1514);
 
+async function verifyInputs(core,branch, tag, ghToken){
+  console.log('Verify inputs')
+  if(!branch.length) {
+      core.setFailed('Branch invalid')
+  }
+  if(!tag.length) {
+      core.setFailed('Tag invalid')
+  }
+  if(!ghToken.length) {
+      core.setFailed('Token is required')
+  }
+  return
+}
 
 async function run(){
     try {
@@ -10986,34 +10999,39 @@ async function run(){
         const tag = core.getInput('tag');
         const ghToken = core.getInput('gh-token');
         const appendTag = core.getInput('append-tag');
+        const upper = core.getBooleanInput('upper');
         const octokit = github.getOctokit(ghToken);
 
-        console.log('branch: ',branch);
-        console.log('tag: ',tag);
-        console.log('gh-token: ',ghToken);
-        console.log('append-tag: ',appendTag);
-        
+        // console.log('branch: ',branch);
+        // console.log('tag: ',tag);
+        // console.log('gh-token: ',ghToken);
+        // console.log('append-tag: ',appendTag);
+        await verifyInputs(core,branch,tag,ghToken);
+        console.log('Get Commit hash')
         const headCommit = await octokit.request(`GET /repos/{owner}/{repo}/commits/${branch}`, {
           owner: github.context.repo.owner,
           repo: github.context.repo.repo
         })
         const commit = headCommit.data.sha
-        console.log('commit actual: ',commit)
-        
+        // console.log('commit actual: ',commit)
+        console.log('Create Release')
         const release = await command.exec('gh',['release','create',`${tag}`,`--title=${tag}`,`--target=${commit}`,'--generate-notes'])
-        console.log('release result: ', release)
+        // console.log('release result: ', release)
+        if(appendTag.length){
+          console.log('Append Tag to commit')
+          const stageTag = upper ? appendTag.toUpperCase() : appendTag
 
-        const messageTag = `add tag - ${appendTag.toUpperCase()} to commit - ${commit}`
-        const userName = await command.exec('git',['config','user.name',`${github.context.actor}`]);
-        console.log('userName: ',userName)
-        const userEmail = await command.exec('git',['config','user.email',`${github.context.actor}@users.noreply.github.com`]);
-        console.log('userEmail: ',userEmail)
-        const stageTag = await command.exec('git',['tag','-a',`${appendTag.toUpperCase()}`,`${commit}`,`-m=${messageTag}`,'-f']);
-        console.log('stageTag: ',stageTag)
-        const setOrigin = await command.exec('git',['remote','set-url','origin',`https://${github.context.actor}:${ghToken}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`]);
-        console.log('setOrigin: ',setOrigin)
-        const forcePush = await command.exec('git',['push','--force','origin',`${appendTag.toUpperCase()}`]);
-        console.log('forcePush: ',forcePush)
+          const messageTag = `add tag - ${stageTag} to commit - ${commit}`
+          const userName = await command.exec('git',['config','user.name',`${github.context.actor}`]);
+          console.log('userName: ',userName)
+          const userEmail = await command.exec('git',['config','user.email',`${github.context.actor}@users.noreply.github.com`]);
+          console.log('userEmail: ',userEmail)
+          await command.exec('git',['tag','-a',`${stageTag}`,`${commit}`,`-m=${messageTag}`,'-f']);
+          const setOrigin = await command.exec('git',['remote','set-url','origin',`https://${github.context.actor}:${ghToken}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`]);
+          console.log('setOrigin: ',setOrigin)
+          const forcePush = await command.exec('git',['push','--force','origin',`${stageTag}`]);
+          console.log('forcePush: ',forcePush)
+        }
     } catch (error) {
       core.setFailed(error.message);
     }
