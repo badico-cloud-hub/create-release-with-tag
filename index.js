ef738c516aa2a5d5f419667f99ad55d3544af54a
wellington gadelha <contato.informeai@gmail.com>
2022-11-01 09:45:45 -0300

    chore(dist/index.js): update order of logs

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run(){
         console.log('userName: ',userName)
         const userEmail = await command.exec('git',['config','user.email',`${github.context.actor}@users.noreply.github.com`]);
         console.log('userEmail: ',userEmail)
-        const stageTag = await command.exec('git',['tag','-f','-a',`${appendTag.toUpperCase()}`,`${commit}`]);
+        const stageTag = await command.exec('git',['tag','-a',`${appendTag.toUpperCase()}`,`${commit}`,`-m=${messageTag}`]);
         console.log('stageTag: ',stageTag)
         const setOrigin = await command.exec('git',['remote','set-url','origin',`https://${github.context.actor}:${ghToken}@github.com/${github.context.repo.repo}.git`]);
         console.log('setOrigin: ',setOrigin)
